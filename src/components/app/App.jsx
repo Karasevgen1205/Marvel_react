1611c60b59d675272a53c3ee499a7a6215d05922
Yauheni Karas <yauhenikaras@ester.co>
2021-12-21 22:30:34 +0300

    fix: get random char and add description

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,22 +3,25 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 
-import decoration from '../../resources/img/vision.png';
+import decoration from "../../resources/img/vision.png";
+import "./app.scss";
 
 const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <RandomChar/>
-                <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
-            </main>
-        </div>
-    )
-}
+  return (
+    <div className="app">
+      <div className="container">
+        <AppHeader />
+        <main>
+          <RandomChar />
+          <div className="char__content">
+            <CharList />
+            <CharInfo />
+          </div>
+          <img className="bg-decoration" src={decoration} alt="vision" />
+        </main>
+      </div>
+    </div>
+  );
+};
 
 export default App;
--- src/components/randomChar/RandomChar.jsx
+++ src/components/randomChar/RandomChar.jsx
@@ -1,75 +1,70 @@
 import { Component } from "react";
-// import thor from "../../resources/img/thor.jpeg";
-import mjolnir from "../../resources/img/mjolnir.png";
 import MarvelService from "../../services/MarvelService";
+
 import "./randomChar.scss";
+import mjolnir from "../../resources/img/mjolnir.png";
 
 class RandomChar extends Component {
-	state = {
-		name: null,
-		description: null,
-		thumbnail: null,
-		homepage: null,
-		wiki: null,
-	};
+  constructor(props) {
+    super(props);
+    this.updateChar();
+  }
 
-	marvelService = new MarvelService();
+  state = {
+    char: {},
+  };
 
-	updateChar = (id) => {
-		this.marvelService.getCharacter(id).then((res) => {
-			// const { name, description, thumbnail, urls } = res.data.results[0];
+  marvelService = new MarvelService();
 
-			console.log(res);
-			// this.setState({
-			// 	name: name,
-			// 	description: description,
-			// 	thumbnail: thumbnail.path + "." + thumbnail.extension,
-			// 	homepage: urls[0].url,
-			// 	wiki: urls[1].url,
-			// });
-		});
-	};
+  onCharLoaded = (char) => {
+    this.setState({ char });
+  };
 
-	render() {
-		const { name, description, thumbnail, homepage, wiki } = this.state;
-		this.updateChar(1011005);
+  updateChar = () => {
+    const id = Math.floor(Math.random() * (1011400 - 1011000) + 1011000);
+    this.marvelService.getCharacter(id).then(this.onCharLoaded);
+  };
 
-		return (
-			<div className="randomchar">
-				<div className="randomchar__block">
-					<img
-						src={thumbnail}
-						alt="Random character"
-						className="randomchar__img"
-					/>
-					<div className="randomchar__info">
-						<p className="randomchar__name">{name}</p>
-						<p className="randomchar__descr">{description}</p>
-						<div className="randomchar__btns">
-							<a href={homepage} className="button button__main">
-								<div className="inner">homepage</div>
-							</a>
-							<a href={wiki} className="button button__secondary">
-								<div className="inner">Wiki</div>
-							</a>
-						</div>
-					</div>
-				</div>
-				<div className="randomchar__static">
-					<p className="randomchar__title">
-						Random character for today!
-						<br />
-						Do you want to get to know him better?
-					</p>
-					<p className="randomchar__title">Or choose another one</p>
-					<button className="button button__main">
-						<div className="inner">try it</div>
-					</button>
-					<img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const {
+      char: { name, description, thumbnail, homepage, wiki },
+    } = this.state;
+    return (
+      <div className="randomchar">
+        <div className="randomchar__block">
+          <img
+            src={thumbnail}
+            alt="Random character"
+            className="randomchar__img"
+          />
+          <div className="randomchar__info">
+            <p className="randomchar__name">{name}</p>
+            <p className="randomchar__descr">{description}</p>
+            <div className="randomchar__btns">
+              <a href={homepage} className="button button__main">
+                <div className="inner">homepage</div>
+              </a>
+              <a href={wiki} className="button button__secondary">
+                <div className="inner">Wiki</div>
+              </a>
+            </div>
+          </div>
+        </div>
+        <div className="randomchar__static">
+          <p className="randomchar__title">
+            Random character for today!
+            <br />
+            Do you want to get to know him better?
+          </p>
+          <p className="randomchar__title">Or choose another one</p>
+          <button className="button button__main">
+            <div className="inner">try it</div>
+          </button>
+          <img src={mjolnir} alt="mjolnir" className="randomchar__decoration" />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default RandomChar;
