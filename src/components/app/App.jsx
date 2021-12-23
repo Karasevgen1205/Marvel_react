f61e3bf8c93d8dd1fa9b456634099c6af7d444ee
Yauheni Karas <yauhenikaras@ester.co>
2021-12-23 21:28:45 +0300

    add errorBoundaary with componentDidCatch

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -3,36 +3,43 @@ import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
+import ErrorBoundary from "../errorBoundary/ErrorBoundary";
 
 import decoration from "../../resources/img/vision.png";
 import "./app.scss";
 
 class App extends Component {
-	state = {
-		selectedChar: null,
-	};
+  state = {
+    selectedChar: null,
+  };
 
-	onCharSelected = (id) => {
-		this.setState({ selectedChar: id });
-	};
+  onCharSelected = (id) => {
+    this.setState({ selectedChar: id });
+  };
 
-	render() {
-		return (
-			<div className="app">
-				<div className="container">
-					<AppHeader />
-					<main>
-						<RandomChar />
-						<div className="char__content">
-							<CharList onCharSelected={this.onCharSelected} />
-							<CharInfo charId={this.state.selectedChar} />
-						</div>
-						<img className="bg-decoration" src={decoration} alt="vision" />
-					</main>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div className="app">
+        <div className="container">
+          <ErrorBoundary>
+            <AppHeader />
+          </ErrorBoundary>
+          <main>
+            <RandomChar />
+            <div className="char__content">
+              <ErrorBoundary>
+                <CharList onCharSelected={this.onCharSelected} />
+              </ErrorBoundary>
+              <ErrorBoundary>
+                <CharInfo charId={this.state.selectedChar} />
+              </ErrorBoundary>
+            </div>
+            <img className="bg-decoration" src={decoration} alt="vision" />
+          </main>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
