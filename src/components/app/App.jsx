983d85fe700100f5718065e3064b75ca29072b76
Yauheni <yauhenikaras@ester.co>
2021-12-23 13:24:34 +0300

    add char list component

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -6,22 +7,32 @@ import CharInfo from "../charInfo/CharInfo";
 import decoration from "../../resources/img/vision.png";
 import "./app.scss";
 
-const App = () => {
-  return (
-    <div className="app">
-      <div className="container">
-        <AppHeader />
-        <main>
-          <RandomChar />
-          <div className="char__content">
-            <CharList />
-            <CharInfo />
-          </div>
-          <img className="bg-decoration" src={decoration} alt="vision" />
-        </main>
-      </div>
-    </div>
-  );
-};
+class App extends Component {
+	state = {
+		selectedChar: null,
+	};
+
+	onCharSelected = (id) => {
+		this.setState({ selectedChar: id });
+	};
+
+	render() {
+		return (
+			<div className="app">
+				<div className="container">
+					<AppHeader />
+					<main>
+						<RandomChar />
+						<div className="char__content">
+							<CharList onCharSelected={this.onCharSelected} />
+							<CharInfo charId={this.state.selectedChar} />
+						</div>
+						<img className="bg-decoration" src={decoration} alt="vision" />
+					</main>
+				</div>
+			</div>
+		);
+	}
+}
 
 export default App;
