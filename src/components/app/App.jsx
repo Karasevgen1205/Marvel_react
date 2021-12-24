d85fefa280d1fd91ab70f68ead77a849cc95e6aa
Yauheni <yauhenikaras@ester.co>
2021-12-24 16:26:15 +0300

    add focus on the cards

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,5 @@
 import { Component } from "react";
+// import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -9,37 +10,69 @@ import decoration from "../../resources/img/vision.png";
 import "./app.scss";
 
 class App extends Component {
-  state = {
-    selectedChar: null,
-  };
+	state = {
+		selectedChar: null,
+	};
 
-  onCharSelected = (id) => {
-    this.setState({ selectedChar: id });
-  };
+	onCharSelected = (id) => {
+		this.setState({ selectedChar: id });
+	};
 
-  render() {
-    return (
-      <div className="app">
-        <div className="container">
-          <ErrorBoundary>
-            <AppHeader />
-          </ErrorBoundary>
-          <main>
-            <RandomChar />
-            <div className="char__content">
-              <ErrorBoundary>
-                <CharList onCharSelected={this.onCharSelected} />
-              </ErrorBoundary>
-              <ErrorBoundary>
-                <CharInfo charId={this.state.selectedChar} />
-              </ErrorBoundary>
-            </div>
-            <img className="bg-decoration" src={decoration} alt="vision" />
-          </main>
-        </div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			// <Router>
+			<div className="app">
+				<div className="container">
+					{/* <nav>
+							<ul>
+								<li>
+									<Link to="/">Home</Link>
+								</li>
+								<li>
+									<Link to="/login">login</Link>
+								</li>
+								<li>
+									<Link to="/recovery-passwor">recovery-passwor</Link>
+								</li>
+							</ul>
+						</nav> */}
+					{/* <Routes>
+							<Route exact path="/" element={<h1>Home</h1>} />
+							<Route
+								exact
+								path="/login"
+								element={
+									<ErrorBoundary>
+										<CharInfo charId={this.state.selectedChar} />
+									</ErrorBoundary>
+								}
+							/>
+							<Route
+								exact
+								path="/recovery-password"
+								element={<h1>recovery-password</h1>}
+							/>
+						</Routes> */}
+					<ErrorBoundary>
+						<AppHeader />
+					</ErrorBoundary>
+					<main>
+						<RandomChar />
+						<div className="char__content">
+							<ErrorBoundary>
+								<CharList onCharSelected={this.onCharSelected} />
+							</ErrorBoundary>
+							<ErrorBoundary>
+								<CharInfo charId={this.state.selectedChar} />
+							</ErrorBoundary>
+						</div>
+						<img className="bg-decoration" src={decoration} alt="vision" />
+					</main>
+				</div>
+			</div>
+			// </Router>
+		);
+	}
 }
 
 export default App;
--- src/components/charInfo/CharInfo.jsx
+++ src/components/charInfo/CharInfo.jsx
@@ -1,4 +1,6 @@
 import { Component } from "react";
+import PropTypes from "prop-types";
+
 import MarvelService from "../../services/MarvelService";
 import Spinner from "../spinner/Spinner";
 import ErrorMessage from "../errorMessage/ErrorMessage";
@@ -6,119 +8,127 @@ import Skeleton from "../skeleton/Skeleton";
 import "./charInfo.scss";
 
 class CharInfo extends Component {
-  state = {
-    char: null,
-    loading: false,
-    error: false,
-  };
+	state = {
+		char: null,
+		loading: false,
+		error: false,
+	};
 
-  marvelService = new MarvelService();
+	marvelService = new MarvelService();
 
-  componentDidMount() {
-    this.updateChar();
-  }
+	componentDidMount() {
+		this.updateChar();
+	}
 
-  componentDidUpdate(prevProps) {
-    if (this.props.charId !== prevProps.charId) {
-      this.updateChar();
-    }
-  }
+	componentDidUpdate(prevProps) {
+		if (this.props.charId !== prevProps.charId) {
+			this.updateChar();
+		}
+	}
 
-  updateChar = () => {
-    const { charId } = this.props;
-    if (!charId) {
-      return;
-    }
+	updateChar = () => {
+		const { charId } = this.props;
+		if (!charId) {
+			return;
+		}
 
-    this.onCharLoading();
-    this.marvelService
-      .getCharacter(charId)
-      .then(this.onCharLoaded)
-      .catch(this.onError);
-  };
+		this.onCharLoading();
+		this.marvelService
+			.getCharacter(charId)
+			.then(this.onCharLoaded)
+			.catch(this.onError);
+	};
 
-  onCharLoaded = (char) => {
-    this.setState({
-      char,
-      loading: false,
-      error: false,
-    });
-  };
+	onCharLoaded = (char) => {
+		this.setState({
+			char,
+			loading: false,
+			error: false,
+		});
+	};
 
-  onCharLoading = () => {
-    this.setState({
-      loading: true,
-      error: false,
-    });
-  };
+	onCharLoading = () => {
+		this.setState({
+			loading: true,
+			error: false,
+		});
+	};
 
-  onError = () => {
-    this.setState({
-      loading: false,
-      error: true,
-    });
-  };
+	onError = () => {
+		this.setState({
+			loading: false,
+			error: true,
+		});
+	};
 
-  render() {
-    const { char, loading, error } = this.state;
-    const skeleton = char || loading || error ? null : <Skeleton />;
-    const errorMessage = error ? <ErrorMessage /> : null;
-    const spinner = loading ? <Spinner /> : null;
-    const content = !(loading || error || !char) ? <View char={char} /> : null;
+	render() {
+		const { char, loading, error } = this.state;
+		const skeleton = char || loading || error ? null : <Skeleton />;
+		const errorMessage = error ? <ErrorMessage /> : null;
+		const spinner = loading ? <Spinner /> : null;
+		const content = !(loading || error || !char) ? <View char={char} /> : null;
 
-    return (
-      <div className="char__info">
-        {skeleton}
-        {errorMessage}
-        {spinner}
-        {content}
-      </div>
-    );
-  }
+		return (
+			<div className="char__info">
+				{skeleton}
+				{errorMessage}
+				{spinner}
+				{content}
+			</div>
+		);
+	}
 }
 
 const View = ({ char }) => {
-  const { name, description, thumbnail, homepage, wiki, comics } = char;
-  let imgStyle = { objectFit: "cover" };
-  if (
-    thumbnail ===
-    "http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
-  ) {
-    imgStyle = { objectFit: "contain" };
-  }
+	const { name, description, thumbnail, homepage, wiki, comics } = char;
+	let imgStyle = { objectFit: "cover" };
+	if (
+		thumbnail ===
+		"http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg"
+	) {
+		imgStyle = { objectFit: "contain" };
+	}
 
-  return (
-    <>
-      <div className="char__basics">
-        <img src={thumbnail} alt={name} style={imgStyle} />
-        <div>
-          <div className="char__info-name">{name}</div>
-          <div className="char__btns">
-            <a href={homepage} className="button button__main">
-              <div className="inner">homepage</div>
-            </a>
-            <a href={wiki} className="button button__secondary">
-              <div className="inner">Wiki</div>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="char__descr">{description}</div>
-      <div className="char__comics">Comics:</div>
-      <ul className="char__comics-list">
-        {comics.length === 0 ? "There are no comics here" : null}
-        {comics.map((item, i) => {
-          if (i < 10) {
-            return (
-              <li className="char__comics-item" key={i}>
-                {item.name}
-              </li>
-            );
-          }
-        })}
-      </ul>
-    </>
-  );
+	return (
+		<>
+			<div className="char__basics">
+				<img src={thumbnail} alt={name} style={imgStyle} />
+				<div>
+					<div className="char__info-name">{name}</div>
+					<div className="char__btns">
+						<a href={homepage} className="button button__main">
+							<div className="inner">homepage</div>
+						</a>
+						<a href={wiki} className="button button__secondary">
+							<div className="inner">Wiki</div>
+						</a>
+					</div>
+				</div>
+			</div>
+			<div className="char__descr">{description}</div>
+			<div className="char__comics">Comics:</div>
+			<ul className="char__comics-list">
+				{comics.length === 0 ? "There are no comics here" : null}
+				{comics.map((item, i) => {
+					if (i < 10) {
+						return (
+							<li className="char__comics-item" key={i}>
+								{item.name}
+							</li>
+						);
+					}
+				})}
+			</ul>
+		</>
+	);
+};
+
+CharInfo.defaultProps = {
+	charId: 210,
+};
+
+CharInfo.propTypes = {
+	charId: PropTypes.number,
 };
 
 export default CharInfo;
