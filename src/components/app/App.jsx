b7410c75e27c05e8261df71d9d58b8e6ee1604ce
Yauheni Karas <yauhenikaras@ester.co>
2021-12-28 21:32:50 +0300

    add home page

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -5,30 +5,32 @@ import Footer from "../footer/Footer";
 import Game from "../appGame/game/Game";
 import AppMarvel from "../appMarvel/AppMarvel";
 import AppEmployeesList from "../appEmployeesList/appEmployeesList/AppEmployeesList";
+import Home from "../home/Home";
 import "./app.scss";
 
 class App extends Component {
-	render() {
-		return (
-			<Router>
-				<div className="all-works">
-					<Header />
-					<div className="app">
-						<Routes>
-							<Route exact path="/tic-tac-toe" element={<Game />} />
-							<Route exact path="/marvel" element={<AppMarvel />} />
-							<Route
-								exact
-								path="/employees-list"
-								element={<AppEmployeesList />}
-							/>
-						</Routes>
-					</div>
-					<Footer />
-				</div>
-			</Router>
-		);
-	}
+  render() {
+    return (
+      <Router>
+        <div className="all-works">
+          <Header />
+          <div className="app">
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/tic-tac-toe" element={<Game />} />
+              <Route exact path="/marvel" element={<AppMarvel />} />
+              <Route
+                exact
+                path="/employees-list"
+                element={<AppEmployeesList />}
+              />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,36 +4,41 @@ import logo from "../../resources/img/Avengers_logo.png";
 import "./header.scss";
 
 const Header = () => {
-	return (
-		<header className="header">
-			<div className="container">
-				<div className="header__wrapper">
-					<div className="header__logo">
-						<img src={logo} alt="Header logo" />
-					</div>
-					<nav className="header__nav">
-						<ul className="header__list">
-							<li className="header__item">
-								<Link to="/marvel" className="header__link">
-									Marvel
-								</Link>
-							</li>
-							<li className="header__item">
-								<Link to="/tic-tac-toe" className="header__link">
-									Tic tac toe
-								</Link>
-							</li>
-							<li className="header__item">
-								<Link to="/employees-list" className="header__link">
-									Employees list
-								</Link>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
-		</header>
-	);
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__logo">
+            <img src={logo} alt="Header logo" />
+          </div>
+          <nav className="header__nav">
+            <ul className="header__list">
+              <li className="header__item">
+                <Link to="/" className="header__link">
+                  Home
+                </Link>
+              </li>
+              <li className="header__item">
+                <Link to="/marvel" className="header__link">
+                  Marvel
+                </Link>
+              </li>
+              <li className="header__item">
+                <Link to="/tic-tac-toe" className="header__link">
+                  Tic tac toe
+                </Link>
+              </li>
+              <li className="header__item">
+                <Link to="/employees-list" className="header__link">
+                  Employees list
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
