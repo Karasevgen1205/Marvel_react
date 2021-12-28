251596c5252c5d61c60dd1b8903d5b776589ef34
Yauheni Karas <yauhenikaras@ester.co>
2021-12-28 21:48:53 +0300

    update footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,7 +3,7 @@ import "./footer.scss";
 const Footer = () => {
   return (
     <div className="footer">
-      <h1>The information is current as of 12/24/2021</h1>
+      <h1>The information is current as of 12/29/2021</h1>
     </div>
   );
 };
