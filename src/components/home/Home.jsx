cf400e4581b97034ecc4175ddf2ff47eff52d078
Yauheni <yauhenikaras@ester.co>
2022-01-06 18:05:37 +0300

    add currency exchange rate

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,41 @@
+import { useEffect, useState } from "react";
+import MarvelService from "../../services/MarvelService";
 import cv from "../../resources/img/CV-2021.svg";
 import "./home.scss";
 
 const Home = () => {
+	const [data, setData] = useState(null);
+	const marvelService = new MarvelService();
+
+	useEffect(() => {
+		marvelService
+			.getResource("https://www.nbrb.by/api/exrates/rates?periodicity=0")
+			.then((res) => {
+				setData(res);
+			});
+	}, []);
+
+	let curs = null;
+
+	if (data) {
+		console.log(data);
+		curs = data.map((item, i) => {
+			return (
+				<div className="block" key={i}>
+					<span>{item.Cur_Abbreviation}</span>
+					<span>{item.Cur_ID}</span>
+					<span>{item.Cur_Name}</span>
+					<span>{item.Cur_OfficialRate}</span>
+					<span>{item.Cur_Scale}</span>
+				</div>
+			);
+		});
+	}
+
 	return (
 		<div className="home">
 			<div className="container">
+				<div className="home__curs">{curs}</div>
 				<div className="home__img">
 					<img src={cv} alt="" />
 				</div>
